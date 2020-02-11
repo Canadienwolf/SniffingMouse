ffc3e76fd0420f298ad70ba4a2f1499b445b63b7
Canadienwolf <39683148+Canadienwolf@users.noreply.github.com>
2020-02-11 15:19:28 +0100

    Bucket Trap with plank is done

---
+++ Assets/_Scripts/Trap Functinalities/DeathOnContact.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathOnContact : MonoBehaviour
+{
+
+    public int timer;
+    
+    public PlayerStatesMovements playerstatesA;
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.tag == "Player")
+        {
+            playerstatesA.lockController = true;
+            
+            Invoke("SceneChange", timer);
+        }
+    }
+
+    void SceneChange()
+    {
+        SceneManager.LoadScene("menu_ScoreDisplay");
+    }
+    
+}
---
+++ Assets/_Scripts/Trap Functinalities/TrapPlank.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapPlank : MonoBehaviour
+{
+    
+    //public
+    public float speed;
+    
+    
+    //private
+    private GameObject trap;
+    private Quaternion rotation1 = Quaternion.Euler(0, 0, 0);
+    private Quaternion rotation2 = Quaternion.Euler(-85, 0, 0);
+
+    private bool falling;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        trap = gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (falling)
+        {
+            transform.localRotation = Quaternion.RotateTowards(transform.localRotation, rotation2, Time.deltaTime * speed);
+        }
+
+        if (!falling)
+        {
+            transform.localRotation = Quaternion.RotateTowards(transform.localRotation, rotation1, Time.deltaTime * speed);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            falling = true;
+        }
+    }
+
+    
+    private void OnTriggerExit(Collider other)
+    {
+        falling = false;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        
+        
+        
+    }
+    
+}
