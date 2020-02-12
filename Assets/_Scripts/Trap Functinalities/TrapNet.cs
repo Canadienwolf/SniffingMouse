b5ef1a92bfad6764e3815423eeb7ffd610ded616
Canadienwolf <39683148+Canadienwolf@users.noreply.github.com>
2020-02-12 15:02:16 +0100

    Integration of FMOD and finished the net trap

--- Assets/_Scripts/Trap Functinalities/TrapNet.cs
+++ Assets/_Scripts/Trap Functinalities/TrapNet.cs
@@ -7,19 +7,40 @@ public class TrapNet : MonoBehaviour
 {
     
     //public
+    [Range(0, 100)]
+    public float speed;
+    
+    [HideInInspector]
     public GameObject trapNet;
     public GameObject cutTrapnet;
 
     //private
+    private GameObject triggerButton;
+    private bool triggerActive;
     
     //-------------------------------------------------------
 
+    private void Start()
+    {
+        triggerButton = gameObject;
+    }
+
+    private void Update()
+    {
+        if (triggerActive)
+        {
+            trapNet.transform.position = Vector3.MoveTowards(trapNet.transform.position, triggerButton.transform.position, Time.deltaTime * speed);
+        }
+
+        if (trapNet == null)
+        {
+            cutTrapnet.SetActive(true);
+            cutTrapnet.transform.position = trapNet.transform.position;
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
-        trapNet.GetComponent<Rigidbody>().useGravity = true;
-        print("cool");
+        triggerActive = true;
     }
-    
-    
-    
 }
