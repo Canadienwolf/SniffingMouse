d786c0dbd681c7df33878b5bcdbe06adfabe8654
Canadienwolf <39683148+Canadienwolf@users.noreply.github.com>
2020-02-26 13:58:07 +0100

    sutff

---
+++ Assets/_Scripts/Trap Functinalities/CableController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CableController : MonoBehaviour
+{
+
+    [HideInInspector]
+    public bool trapActive;
+
+    private void Awake()
+    {
+        trapActive = true;
+        
+    }
+
+    private void Update()
+    {
+        if (trapActive = false)
+        {
+            //GetComponentInChildren<ElectricCableTrap>();
+        }
+    }
+}
