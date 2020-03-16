f827c828916d1e69964935d5616dc620a4fb0077
Hafrass <sindreva3@gmail.com>
2020-03-16 14:45:08 +0100

    Made a drop shadow for the UFO and the player

---
+++ Assets/_Scripts/DropShadow.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropShadow : MonoBehaviour
+{
+    void Update()
+    {
+        RaycastHit hitInfo;
+        if (Physics.Raycast(transform.position, Vector3.down, out hitInfo, 1000))
+        {
+            transform.GetChild(0).transform.position = hitInfo.point + new Vector3(0, 0.01f, 0);
+        }
+    }
+}
