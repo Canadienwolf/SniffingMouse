4565747a66321370e78ff5cd44c50c2b39d82f1f
Canadienwolf <39683148+Canadienwolf@users.noreply.github.com>
2020-04-20 11:39:14 +0200

    fix

---
+++ Assets/_Scripts/Objects/Cupboard/DoorBhaviour.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorBhaviour : MonoBehaviour
+{
+
+    public Animator doorAnimation;
+
+
+    public int doorPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        doorPosition = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            print("1");
+            
+            /*
+            if (doorPosition == 2)
+            {
+                print("2");
+                doorAnimation.SetTrigger("Close");
+                doorPosition = 1;
+            }
+            */
+
+            if (doorPosition == 1)
+            {
+                print("3");
+                doorAnimation.SetTrigger("Open");
+                doorPosition = 2;
+            }
+            
+            
+        }
+    }
+}
