d2b9c061da11dfb2bcb1cec22ae1eecb9af2714a
Canadienwolf <39683148+Canadienwolf@users.noreply.github.com>
2020-04-23 21:58:53 +0200

    Falling Net SFX
    Falling net now has destruction sound when the knife hits it.

---
+++ Assets/_Scripts/Audio/SFXController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXController : MonoBehaviour
+{
+    
+    public GameObject SFX;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        SFX.SetActive(true);
+    }
+}
