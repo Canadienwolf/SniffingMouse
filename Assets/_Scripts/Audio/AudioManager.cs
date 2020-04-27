67033a2c6e1f0110ec339c11b75d00eb7e35aac3
Hafrass <sindreva3@gmail.com>
2020-04-27 13:18:43 +0200

    Sound slider fix

--- Assets/_Scripts/Audio/AudioManager.cs
+++ Assets/_Scripts/Audio/AudioManager.cs
@@ -9,41 +9,27 @@ public class AudioManager : MonoBehaviour
     FMOD.Studio.EventInstance SFXVolumeTestEvent;
 
     //Game Managers (Scriptable Object)
-    public AudioSettings GSAudioManager;
+    //public AudioSettings GSAudioManager;
     
     //Fmod connections
     public FMOD.Studio.Bus Music;
     public FMOD.Studio.Bus SFX;
     public FMOD.Studio.Bus Master;
     
+
+
     //Volume controls
-    public float MusicVolume;
-    public Slider musicSlider;
-    public float SFXVolume;
-    public Slider SFXSlider;
-    public float MasterVolume;
-    public Slider MasterSlider;
+    public static float MusicVolume;
+    public static float SFXVolume;
+    public static float MasterVolume;
 
     private void Awake()
     {
         DontDestroyOnLoad(this);
-        
+
         Music = FMODUnity.RuntimeManager.GetBus("bus:/Master/Music");
         SFX = FMODUnity.RuntimeManager.GetBus("bus:/Master/SFX");
         Master = FMODUnity.RuntimeManager.GetBus("bus:/Master");
-
-        MasterVolume = GSAudioManager.MasterVolume;
-        MusicVolume = GSAudioManager.MusicVolume;
-        SFXVolume = GSAudioManager.SFXVolume;
-        
-        MusicVolume = PlayerPrefs.GetFloat("MusicVolume", MusicVolume);
-        MasterVolume = PlayerPrefs.GetFloat("MasterVolume", MasterVolume);
-        SFXVolume = PlayerPrefs.GetFloat("SFXVolume", SFXVolume);
-    }
-
-    void Start()
-    {
-        
     }
 
     // Update is called once per frame
@@ -52,47 +38,5 @@ public class AudioManager : MonoBehaviour
         Music.setVolume(MusicVolume);
         SFX.setVolume(SFXVolume);
         Master.setVolume(MasterVolume);
-        
-        //Finding the slider in the pause menu in each level
-        musicSlider = GameObject.FindGameObjectWithTag("musicSlider").GetComponent<Slider>();
-        SFXSlider = GameObject.FindGameObjectWithTag("SFXSlider").GetComponent<Slider>();
-        MasterSlider = GameObject.FindGameObjectWithTag("masterSlider").GetComponent<Slider>();
-        
-        
-        musicSlider.value = GSAudioManager.MusicVolume;
-        SFXSlider.value = GSAudioManager.SFXVolume;
-        MasterSlider.value = GSAudioManager.MasterVolume;
-    }
-    
-    public void MasterVolumeLevel (float newMasterVolume)
-    {
-        MasterVolume = newMasterVolume;
-        PlayerPrefs.SetFloat("MasterVolume", MasterVolume);
-        GSAudioManager.MasterVolume = newMasterVolume;
-    }
-
-    public void MusicVolumeLevel (float newMusicVolume)
-    {
-        MusicVolume = newMusicVolume;
-        PlayerPrefs.SetFloat("MusicVolume", MusicVolume);
-        GSAudioManager.MusicVolume = newMusicVolume;
-    }
-
-    public void SFXVolumeLevel (float newSFXVolume)
-    {
-        SFXVolume = newSFXVolume;
-        PlayerPrefs.SetFloat("SFXVolume", SFXVolume);
-        GSAudioManager.SFXVolume = newSFXVolume;
-
-        /*
-        FMOD.Studio.PLAYBACK_STATE PbState;
-        SFXVolumeTestEvent.getPlaybackState (out PbState);
-        if (PbState != FMOD.Studio.PLAYBACK_STATE.PLAYING) 
-        {
-            SFXVolumeTestEvent.start ();
-        }
-        */
-    }
-    
-    
+    }    
 }
---
+++ Assets/_Scripts/Audio/AudioSliderManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioSliderManager : MonoBehaviour
+{
+    public Slider musicSlider;
+    public Slider SFXSlider;
+    public Slider MasterSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        musicSlider.value = AudioManager.MusicVolume;
+        SFXSlider.value = AudioManager.SFXVolume;
+        MasterSlider.value = AudioManager.MasterVolume;
+
+        MasterSlider.value = PlayerPrefs.GetFloat("Master");
+        SFXSlider.value = PlayerPrefs.GetFloat("SFX");
+        musicSlider.value = PlayerPrefs.GetFloat("Music");
+    }
+
+    public void ChangeMaster()
+    {
+        AudioManager.MasterVolume = MasterSlider.value;
+        PlayerPrefs.SetFloat("Master", AudioManager.MasterVolume);
+    }
+
+    public void ChangeSFX()
+    {
+        AudioManager.SFXVolume = SFXSlider.value;
+        PlayerPrefs.SetFloat("SFX", AudioManager.SFXVolume);
+    }
+
+    public void ChangeMusic()
+    {
+        AudioManager.MusicVolume = musicSlider.value;
+        PlayerPrefs.SetFloat("Music", AudioManager.MusicVolume);
+    }
+}
