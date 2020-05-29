2144e0bb5669f8b4128a19ad7f22ccbe2c9e6602
Canadienwolf <39683148+Canadienwolf@users.noreply.github.com>
2020-05-29 16:50:07 +0200

    Music in all levels

--- Assets/_Scripts/Audio/AudioManager.cs
+++ Assets/_Scripts/Audio/AudioManager.cs
@@ -3,6 +3,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
 
 public class AudioManager : MonoBehaviour
 {
@@ -10,7 +12,7 @@ public class AudioManager : MonoBehaviour
 
     //Game Managers (Scriptable Object)
     //public AudioSettings GSAudioManager;
-    
+
     //Fmod connections
     public FMOD.Studio.Bus Music;
     public FMOD.Studio.Bus SFX;
@@ -22,9 +24,15 @@ public class AudioManager : MonoBehaviour
     public static float MasterVolume;
     public static AudioManager instance;
 
+    //gameobjects that contatin the music that is to be palyed in levels
+    public GameObject[] MusicPerLevel;
+    private int _activeScene;
+    private int _songIndex;
+
     private void Awake()
     {
-        
+
+
         if (instance != null)
         {
             Destroy(gameObject);
@@ -33,7 +41,7 @@ public class AudioManager : MonoBehaviour
         {
             instance = this;
         }
-        
+
         DontDestroyOnLoad(this);
 
         Music = FMODUnity.RuntimeManager.GetBus("bus:/Master/Music");
@@ -41,18 +49,36 @@ public class AudioManager : MonoBehaviour
         Master = FMODUnity.RuntimeManager.GetBus("bus:/Master");
     }
 
-    
-    
-    private void Start()
+    // Update is called once per frame
+    private void OnEnable()
     {
-       
+        SceneManager.sceneLoaded += ChangeSong;
+    }
+    
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= ChangeSong;
     }
 
-    // Update is called once per frame
-    void Update()
+    void ChangeSong(Scene scene, LoadSceneMode mode)
     {
-        Music.setVolume(MusicVolume);
-        SFX.setVolume(SFXVolume);
-        Master.setVolume(MasterVolume);
+        _activeScene = SceneManager.GetActiveScene().buildIndex;
+        
+        for (int i = 0; i < MusicPerLevel.Length; i++)
+        {
+            MusicPerLevel[i].SetActive(false);
+        }
+
+        if (_activeScene == 0 || _activeScene == 1 || _activeScene == 12 || _activeScene == 13)
+        {
+            _songIndex = 0;
+        }
+        
+        else
+        {
+            _songIndex = Random.Range(1, MusicPerLevel.Length);
+        }
+
+        MusicPerLevel[_songIndex].SetActive(true);
     }
 }
