bc4f84bc08f6b1d98645beffb4f499477ff7dffd
Hafrass <sindreva3@gmail.com>
2020-06-03 13:54:32 +0200

    Revert "Revert "lvl_1 and lvl_2 tweaking""
    This reverts commit a12d8bdf258a480e480bb70c0e691f99cf68bdaa.

---
+++ Assets/_Scripts/Audio/FootSteps.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootSteps : MonoBehaviour
+{
+    [FMODUnity.EventRef] 
+    public string inputSound;
+    public float walkingSpeed;
+    
+    private bool _playerIsMoving;
+
+    private void Update()
+    {
+        if (Input.GetAxis("Vertical") >= 0.01f || Input.GetAxis("Horizontal") >= 0.01f || Input.GetAxis("Vertical") <= -0.01f || Input.GetAxis("Horizontal") <= -0.01f)
+        {
+            Debug.Log("Player is moving");
+            _playerIsMoving = true;
+        }
+        
+        else if (Input.GetAxis("Vertical") == 0 || Input.GetAxis("Horizontal") == 0)
+        {
+            Debug.Log("Player is not moving");
+            _playerIsMoving = false;
+        }
+    }
+
+    void CallFootsteps()
+    {
+        Debug.Log("Player is moving");
+        FMODUnity.RuntimeManager.PlayOneShot(inputSound);
+    }
+
+    private void Start()
+    {
+        InvokeRepeating("CallFootsteps", 0, walkingSpeed);
+    }
+
+    private void OnDisable()
+    {
+        _playerIsMoving = false;
+    }
+}
