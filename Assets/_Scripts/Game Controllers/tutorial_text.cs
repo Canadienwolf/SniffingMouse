c6a958c7a67f8fe19a548eab197d8cc7b4fbb39f
Hafrass <sindreva3@gmail.com>
2020-06-03 14:07:46 +0200

    tutorial_text

---
+++ Assets/_Scripts/Game Controllers/tutorial_text.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class tutorial_text : MonoBehaviour
+{
+    [TextArea]
+    [SerializeField] string tut_txt;
+
+    Text txt;
+
+    private void Start()
+    {
+        txt = GameObject.Find("Tutorial_Text").GetComponent<Text>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            txt.text = tut_txt;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            txt.text = "";
+        }
+    }
+}
